832e2b73b8a4bfc0d6ff314a11ee85740d07494f
Nicholas <quackshows11@gmail.com>
2024-12-02 11:14:52 +0100

    Added GetMapping and DeleteMapping in CustomBookController. Youcan now find books by id and delete books by id

--- src/main/kotlin/com/clemond/laboration_1_crud_applikation/controller/CustomBookController.kt
+++ src/main/kotlin/com/clemond/laboration_1_crud_applikation/controller/CustomBookController.kt
@@ -3,9 +3,12 @@ package com.clemond.laboration_1_crud_applikation.controller
 import com.clemond.laboration_1_crud_applikation.model.CustomBook
 import com.clemond.laboration_1_crud_applikation.repository.CustomBookRepository
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -20,6 +23,36 @@ class CustomBookController(
         customBookRepository.save(customBook)
 
         return ResponseEntity.status(201).body("Book Created")
+    }
+
+    @GetMapping
+    fun getBookById(@RequestParam("id") id: Long): ResponseEntity<CustomBook>{
+
+        val foundOptionalBook = customBookRepository.findById(id)
+
+        if (foundOptionalBook.isPresent) {
+            val notOptionalBook = foundOptionalBook.get()
+
+            return ResponseEntity.status(200).body(notOptionalBook)
+        }
+
+        return ResponseEntity.notFound().build()
+    }
+
+    @DeleteMapping
+    fun deleteBookById(@RequestParam("id") id: Long): ResponseEntity<String> {
+        val foundOptionalBook = customBookRepository.findById(id)
+
+        if (foundOptionalBook.isPresent) {
+            val notOptionalBook = foundOptionalBook.get()
+
+            customBookRepository.delete(notOptionalBook)
+
+            return ResponseEntity.status(200).body("Book deleted")
+
+        }
+
+        return ResponseEntity.notFound().build()
     }
 
 }
