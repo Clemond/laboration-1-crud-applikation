db7a0dc1123a0e6f4ae1c70f68750f4a748d5b93
Nicholas <quackshows11@gmail.com>
2024-12-02 10:28:53 +0100

    Created CustomBookRepository

--- src/main/kotlin/com/clemond/laboration_1_crud_applikation/repository/CustomBookRepository.kt
+++ src/main/kotlin/com/clemond/laboration_1_crud_applikation/repository/CustomBookRepository.kt
@@ -1,4 +1,9 @@
 package com.clemond.laboration_1_crud_applikation.repository
 
-interface CustomBookRepository {
+import com.clemond.laboration_1_crud_applikation.model.CustomBook
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomBookRepository: JpaRepository<CustomBook, Long>{
 }
